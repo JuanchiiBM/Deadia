a93c01057622ab146b11ab51682cc94ccfd5d4d1
jbenitez <juanchibenitez65@gmail.com>
2024-08-22 09:55:44 -0300

    restore layout

--- components/auth/authLayout.tsx
+++ components/auth/authLayout.tsx
@@ -8,10 +8,10 @@ interface Props {
 export const AuthLayoutWrapper = ({ children }: Props) => {
   return (
     <div className='flex h-screen'>
-      <div className='flex-1 flex-col h-screen flex items-center justify-center p-6'>
-        <div className='md:hidden absolute left-0 h-screen right-0 bottom-0 top-0 z-0'>
+      <div className='flex-1 flex-col flex items-center justify-center p-6'>
+        <div className='md:hidden absolute left-0 right-0 bottom-0 top-0 z-0'>
           <Image
-            className='h-full'
+            className='w-full h-full'
             src='https://nextui.org/gradients/docs-right.png'
             alt='gradient'
           />
@@ -23,11 +23,22 @@ export const AuthLayoutWrapper = ({ children }: Props) => {
         <Divider orientation='vertical' />
       </div>
 
-      <div className='hidden md:flex flex-1 h-screen relative flex items-center justify-center p-6'>
+      <div className='hidden md:flex flex-1 relative flex items-center justify-center p-6'>
+        <div className='absolute left-0 right-0 bottom-0 top-0 z-0'>
+          <Image
+            className='w-full h-full'
+            src='https://nextui.org/gradients/docs-right.png'
+            alt='gradient'
+          />
+        </div>
 
         <div className='z-10'>
-          <h1 className='font-bold text-[45px]'>DEADIA</h1>
+          <h1 className='font-bold text-[45px]'>NextUI Dashboard Template</h1>
           <div className='font-light text-slate-400 mt-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+            possimus voluptate, sapiente assumenda deserunt repellendus,
+            perferendis odit voluptas hic dolores laborum fugit ut? Architecto
+            quo ex quidem vitae quae rem.
           </div>
         </div>
       </div>
