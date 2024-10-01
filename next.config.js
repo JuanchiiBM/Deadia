72eaebf5750bb3ff471f301a51bf21e96d4120d2
jbenitez <juanchibenitez65@gmail.com>
2024-10-01 09:06:43 -0300

    Fix millionlint desactivado hasta que termine su beta

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-const MillionLint = require('@million/lint');
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ['three'],
+};
+
+module.exports = nextConfig;
+
+/*
+const MillionLint = require('@million/lint');
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['three']
 };
 module.exports = MillionLint.next()(nextConfig);
+*/
